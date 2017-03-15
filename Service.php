d690ffc5752be407983182a2c4965a3deb0808cb
zhanglong <zhanglong@jcy.cc>
2017-03-15 18:12:22 +0800

    修改service传参规则   必须严格按照先必填后选填的规则来

--- Service.php
+++ Service.php
@@ -65,9 +65,11 @@ class Service
                 elseif(isset($params[$arg->name])){
                     $act_params[]=$params[$arg->name];
                     unset($params[$arg->name]);
-                }else
-                    $act_params[]=null;
+                }
+//                else
+//                    $act_params[]=[];
             }
+            
             if($act_number_required_params>count($act_params))
                 return pushFailedMsg('服务接口参数不够.');
         }
